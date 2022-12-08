cb5b59775ce469cb7c9662a5cc8ef108c365b94d
williamtessa27 <williamtessa270794@gmail.com>
2022-12-08 02:10:17 +0000

    first commit

---
+++ src/main/java/com/tessa/gestiondestock/model/AbstractEntity.java
@@ -0,0 +1,35 @@
+package com.tessa.gestiondestock.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+
+@Data
+@RequiredArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @CreatedDate
+    @Column(name = "creation", nullable = false)
+    @JsonIgnore
+    private Instant creationDate;
+
+    @LastModifiedDate
+    @Column(name = "lastModifiedDate")
+    @JsonIgnore
+    private Instant lastModifiedDate;
+}
